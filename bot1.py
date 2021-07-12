c398d8a62ea045f201710b11148f8ab62b98ffb1
msafari2001 <87254697+msafari2001@users.noreply.github.com>
2021-07-12 12:52:39 +0430

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -35,40 +35,42 @@ def index():
         text = msg['message'].get('text', '')
         name = msg['message']['from']['first_name']
         if text=='/start':
-            sendMessage(chat_id,f'salam {name}\nلطفا به فرمت زیر وارد کنید\nwiki +(fa,en)+topik')
+            sendMessage(chat_id,f'salam {name}\nخوش آمدید\nدر این بات می تونید موضوع مورد نظر خود را وارد کنید \nومطالبی در مورد موضوع دریافت کنید و برای مطالعه بیشتر یک لینک می توانید دریافت کنید ')
             return Response('ok', status=200)
-        elif 'wiki fa' in text:
-            m=text.split(maxsplit=2)[2]
+            sendMessage(chat_id,f'ابتدا زبان مدنظر را وارد کنید\nبرای فارسیfa\nبرای انگلیسیen\nبا یک فاصله موضوع مدنظر را وارد کنید\nبرای مثال:\nen iran, fa تهران')
+            return Response('ok', status=200)
+        elif 'fa' in text:
+            m=text.split(maxsplit=1)[1]
             wiki_wiki = wikipediaapi.Wikipedia('fa')
             page_py = wiki_wiki.page(m)
             if page_py.exists()==True:
                 wikipedia.set_lang("fa")
                 try:
                     c=wikipedia.search(m,3)
-                    x=wikipedia.summary(c[0],sentences=5)
+                    x=wikipedia.summary(c[0],sentences=6)
                     y=page_py.fullurl
                 except wikipedia.exceptions.DisambiguationError as e:
                     s=random.choice(e.options)
-                    x=wikipedia.summary(s,sentences=5)
+                    x=wikipedia.summary(s,sentences=6)
                     y=page_py.fullurl
                 sendMessage(chat_id,f'{x}\n{y}')
                 return Response('ok', status=200)
             elif page_py.exists()==False:
                 sendMessage(chat_id,'This topic was not found')
                 return Response('ok', status=200)
-        elif 'wiki en' in text:
-            m=text.split(maxsplit=2)[2]
+        elif 'en' in text:
+            m=text.split(maxsplit=1)[1]
             wiki_wiki = wikipediaapi.Wikipedia('en')
             page_py = wiki_wiki.page(m)
             if page_py.exists()==True:
                 wikipedia.set_lang("en")
                 try:
                     c=wikipedia.search(m,3)
-                    x=wikipedia.summary(c[0],sentences=5)
+                    x=wikipedia.summary(c[0],sentences=6)
                     y=page_py.fullurl
                 except wikipedia.exceptions.DisambiguationError as e:
                     s=random.choice(e.options)
-                    x=wikipedia.summary(s,sentences=5)
+                    x=wikipedia.summary(s,sentences=6)
                     y=page_py.fullurl
                 sendMessage(chat_id,f'{x}\n{y}')
                 return Response('ok', status=200)
@@ -76,7 +78,7 @@ def index():
                 sendMessage(chat_id,'This topic was not found')
                 return Response('ok', status=200)    
         else:
-            sendMessage(chat_id,f'ببخشید!!\nلطفا به صورت زیر وارد کنید\nwiki +(fa,en)+topic\nبرای مثال: wiki en iran')
+            sendMessage(chat_id,f'ببخشید!!\nلطفا به صورت زیر وارد کنید\\nبرای مثال: en iran')
             return Response('ok', status=200)
     else:
         return "<h1>salam</h1>"              
